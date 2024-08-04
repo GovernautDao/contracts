3f9f9249d1cdde2973434c0d023e6c49c5cc7b18
Jaskaran Singh <jaskaransingh@Jaskarans-Laptop.local>
2024-08-04 18:34:27 +0530

    resolved

--- src/Identity Management/IdentityManager.sol
+++ src/Identity Management/IdentityManager.sol
@@ -7,9 +7,9 @@ pragma solidity 0.8.24;
  * @notice Contract will handle identity verification using World ID on #Superchain.
  * It will store mappings of user addresses to unique identifiers provided by World ID, ensuring privacy and security.
  */
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import {ByteHasher} from "./helpers/ByteHasher.sol";
-import {IWorldID} from "../interfaces/IWorldID.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { ByteHasher } from "./helpers/ByteHasher.sol";
+import { IWorldID } from "../interfaces/IWorldID.sol";
 
 contract IdentityManager is Ownable {
     using ByteHasher for bytes;
@@ -30,7 +30,8 @@ contract IdentityManager is Ownable {
     /// @dev The World ID group ID (always 1)
     uint256 internal immutable groupId = 1;
 
-    /// @dev Whether a nullifier hash has been used already. Used to guarantee an action is only performed once by a single person
+    /// @dev Whether a nullifier hash has been used already. Used to guarantee an action is only performed once by a
+    /// single person
     mapping(uint256 => bool) internal nullifierHashes;
 
     /// @param nullifierHash The nullifier hash for the verified proof
@@ -45,36 +46,29 @@ contract IdentityManager is Ownable {
         IWorldID _worldId,
         string memory _appId,
         string memory _actionId
-    ) Ownable(initialOwner) {
+    )
+        Ownable(initialOwner)
+    {
         worldId = _worldId;
-        externalNullifier = abi
-            .encodePacked(abi.encodePacked(_appId).hashToField(), _actionId)
-            .hashToField();
+        externalNullifier = abi.encodePacked(abi.encodePacked(_appId).hashToField(), _actionId).hashToField();
     }
 
-    /// @param signal An arbitrary input from the user, usually the user's wallet address (check README for further details)
+    /// @param signal An arbitrary input from the user, usually the user's wallet address (check README for further
+    /// details)
     /// @param root The root of the Merkle tree (returned by the JS widget).
     /// @param nullifierHash The nullifier hash for this proof, preventing double signaling (returned by the JS widget).
-    /// @param proof The zero-knowledge proof that demonstrates the claimer is registered with World ID (returned by the JS widget).
+    /// @param proof The zero-knowledge proof that demonstrates the claimer is registered with World ID (returned by the
+    /// JS widget).
     /// @dev Feel free to rename this method however you want! We've used `claim`, `verify` or `execute` in the past.
-    function verifyAndExecute(
-        address signal,
-        uint256 root,
-        uint256 nullifierHash,
-        uint256[8] calldata proof
-    ) public {
+    function verifyAndExecute(address signal, uint256 root, uint256 nullifierHash, uint256[8] calldata proof) public {
         // First, we make sure this person hasn't done this before
-        if (nullifierHashes[nullifierHash])
+        if (nullifierHashes[nullifierHash]) {
             revert DuplicateNullifier(nullifierHash);
+        }
 
         // We now verify the provided proof is valid and the user is verified by World ID
         worldId.verifyProof(
-            root,
-            groupId,
-            abi.encodePacked(signal).hashToField(),
-            nullifierHash,
-            externalNullifier,
-            proof
+            root, groupId, abi.encodePacked(signal).hashToField(), nullifierHash, externalNullifier, proof
         );
 
         // We now record the user has done this, so they can't do it again (proof of uniqueness)
