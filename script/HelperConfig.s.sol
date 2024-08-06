07d99388bf19df7c87afdd6529d8d74c585666d7
Jaskaran Singh <jaskaransingh@Jaskarans-Laptop.local>
2024-08-06 20:20:30 +0530

    fmt

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -1,71 +1,61 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-import {Script} from "forge-std/Script.sol";
-import {console} from "forge-std/Test.sol";
-import {WorldIDIdentityManagerRouterMock} from "../test/Anvil/Unit/mocks/WorldIDIdentityManagerRouterMock.sol";
+import { Script } from "forge-std/Script.sol";
+import { console } from "forge-std/Test.sol";
+import { WorldIDIdentityManagerRouterMock } from "../test/Anvil/Unit/mocks/WorldIDIdentityManagerRouterMock.sol";
 
 contract HelperConfig is Script {
-  string appId = vm.envString("WORLDCOIN_APP_ID");
-  string actionId = vm.envString("WORLDCOIN_ACTION_ID");
-  struct Config {
-    string _appid;
-    string _actionId;
-    address _WorldcoinRouterAddress;
-  }
+    string appId = vm.envString("WORLDCOIN_APP_ID");
+    string actionId = vm.envString("WORLDCOIN_ACTION_ID");
 
-  function getOpMainnetConfig() public view returns (Config memory) {
-    Config memory OpMainnetConfig = Config({
-      _appid: appId,
-      _actionId: actionId,
-      _WorldcoinRouterAddress: vm.envAddress(
-        "WORLDCOIN_ROUTERADDRESS_OP_MAINNET"
-      )
-    });
-    return OpMainnetConfig;
-  }
+    struct Config {
+        string _appid;
+        string _actionId;
+        address _WorldcoinRouterAddress;
+    }
 
-  function getOpSepoliaConfig() public view returns (Config memory) {
-    Config memory OpSepoliaConfig = Config({
-      _appid: appId,
-      _actionId: actionId,
-      _WorldcoinRouterAddress: vm.envAddress(
-        "WORLDCOIN_ROUTERADDRESS_OP_SEPOLIA"
-      )
-    });
-    return OpSepoliaConfig;
-  }
+    function getOpMainnetConfig() public view returns (Config memory) {
+        Config memory OpMainnetConfig = Config({
+            _appid: appId,
+            _actionId: actionId,
+            _WorldcoinRouterAddress: vm.envAddress("WORLDCOIN_ROUTERADDRESS_OP_MAINNET")
+        });
+        return OpMainnetConfig;
+    }
 
-  function getEthMainnetConfig() public view returns (Config memory) {
-    Config memory EthMainnetConfig = Config({
-      _appid: appId,
-      _actionId: actionId,
-      _WorldcoinRouterAddress: vm.envAddress(
-        "WORLDCOIN_ROUTERADDRESS_ETH_MAINNET"
-      )
-    });
-    return EthMainnetConfig;
-  }
+    function getOpSepoliaConfig() public view returns (Config memory) {
+        Config memory OpSepoliaConfig = Config({
+            _appid: appId,
+            _actionId: actionId,
+            _WorldcoinRouterAddress: vm.envAddress("WORLDCOIN_ROUTERADDRESS_OP_SEPOLIA")
+        });
+        return OpSepoliaConfig;
+    }
 
-  function getEthSepoliaConfig() public view returns (Config memory) {
-    Config memory EthSepoliaConfig = Config({
-      _appid: appId,
-      _actionId: actionId,
-      _WorldcoinRouterAddress: vm.envAddress(
-        "WORLDCOIN_ROUTERADDRESS_ETH_SEPOLIA"
-      )
-    });
-    return EthSepoliaConfig;
-  }
+    function getEthMainnetConfig() public view returns (Config memory) {
+        Config memory EthMainnetConfig = Config({
+            _appid: appId,
+            _actionId: actionId,
+            _WorldcoinRouterAddress: vm.envAddress("WORLDCOIN_ROUTERADDRESS_ETH_MAINNET")
+        });
+        return EthMainnetConfig;
+    }
 
-  function getAnvilConfig() public returns (Config memory) {
-    console.log("testing on anvil");
-    WorldIDIdentityManagerRouterMock routerMock = new WorldIDIdentityManagerRouterMock();
-    Config memory AnvilConfig = Config({
-      _appid: appId,
-      _actionId: actionId,
-      _WorldcoinRouterAddress: address(routerMock)
-    });
-    return AnvilConfig;
-  }
+    function getEthSepoliaConfig() public view returns (Config memory) {
+        Config memory EthSepoliaConfig = Config({
+            _appid: appId,
+            _actionId: actionId,
+            _WorldcoinRouterAddress: vm.envAddress("WORLDCOIN_ROUTERADDRESS_ETH_SEPOLIA")
+        });
+        return EthSepoliaConfig;
+    }
+
+    function getAnvilConfig() public returns (Config memory) {
+        console.log("testing on anvil");
+        WorldIDIdentityManagerRouterMock routerMock = new WorldIDIdentityManagerRouterMock();
+        Config memory AnvilConfig =
+            Config({ _appid: appId, _actionId: actionId, _WorldcoinRouterAddress: address(routerMock) });
+        return AnvilConfig;
+    }
 }
--- test/Anvil/Unit/TestGovernautGovernance.t.sol
+++ test/Anvil/Unit/TestGovernautGovernance.t.sol
@@ -1,29 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-import {Test, console2} from "forge-std/Test.sol";
-import {GovernautGovernance} from "../../../src/Governance Tools/GovernautGovernance.sol";
-import {MockVotes} from "./mocks/MockVotes.sol";
-import {MockTimelockController} from "./mocks/MockTimelockController.sol";
+import { Test, console2 } from "forge-std/Test.sol";
+import { GovernautGovernance } from "../../../src/Governance Tools/GovernautGovernance.sol";
+import { MockVotes } from "./mocks/MockVotes.sol";
+import { MockTimelockController } from "./mocks/MockTimelockController.sol";
 
 contract TestGovernautGovernance is Test {
-  GovernautGovernance governautGovernance;
-  MockVotes mockVotes;
-  MockTimelockController mockTimelockController;
+    GovernautGovernance governautGovernance;
+    MockVotes mockVotes;
+    MockTimelockController mockTimelockController;
 
-  address IDENTITYMANAGER_ADDRESS = makeAddr("Identity Manager Address");
+    address IDENTITYMANAGER_ADDRESS = makeAddr("Identity Manager Address");
 
-  function setUp() public {
-    // Deploy mock contracts
-    mockVotes = new MockVotes();
-    mockTimelockController = new MockTimelockController();
+    function setUp() public {
+        // Deploy mock contracts
+        mockVotes = new MockVotes();
+        mockTimelockController = new MockTimelockController();
 
-    governautGovernance = new GovernautGovernance(
-      mockVotes,
-      mockTimelockController,
-      IDENTITYMANAGER_ADDRESS
-    );
-  }
+        governautGovernance = new GovernautGovernance(mockVotes, mockTimelockController, IDENTITYMANAGER_ADDRESS);
+    }
 
-  function testDefault() public {}
+    function testDefault() public { }
 }
