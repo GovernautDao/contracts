0a60ef8d004f8d7f19f1a95de0880000a5516fb8
Riiz0 <shawnrizo@hotmail.com>
2024-08-06 02:02:02 -0400

    Feat: Modify TestFunding and Create Mocks for Test

---
+++ test/mocks/MockGovernance.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+interface IGovernautGovernance {
+    function approvedProposers(address proposer) external view returns (bool);
+}
+
+contract MockGovernance is IGovernautGovernance {
+    mapping(address => bool) private _approvedProposers;
+
+    function approvedProposers(address proposer) external view override returns (bool) {
+        return _approvedProposers[proposer];
+    }
+
+    function approveProposer(address proposer) external {
+        _approvedProposers[proposer] = true;
+    }
+
+    function revokeProposerApproval(address proposer) external {
+        _approvedProposers[proposer] = false;
+    }
+}
