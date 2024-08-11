b635cfc8d2e0a8c54ea88b4532b2af91f2ebfbae
Jaskaran Singh <jaskaransingh@Jaskarans-Laptop.local>
2024-08-11 14:11:17 +0530

    staged changes

---
+++ script/Deploy_MetalL2Testnet.s.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {console} from "forge-std/console.sol";
+import "forge-std/Script.sol";
+import "../src/Governance Tools/GovernautGovernance.sol";
+import "../src/Identity Management/IdentityManager.sol";
+import "../src/Onchain Funding/Funding.sol";
+import {IVotes} from "@openzeppelin/contracts/governance/utils/IVotes.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
+import {HelperConfig} from "../script/HelperConfig.s.sol";
+
+contract MyScript is Script {
+  HelperConfig helperConfig;
+  IdentityManager identitymanager;
+  Funding funding;
+  GovernautGovernance governance;
+
+  function run() external {
+    uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+    helperConfig = new HelperConfig();
+    address governancetoken = helperConfig
+      .getOpSepoliaConfig()
+      ._governanceTokenForThatChain;
+    console.log(governancetoken);
+    vm.startBroadcast(deployerPrivateKey);
+
+    // Deploy the IdentityManager contract first
+    identitymanager = new IdentityManager(
+      vm.addr(deployerPrivateKey),
+      helperConfig.getOpSepoliaConfig()._WorldcoinRouterAddress,
+      helperConfig.getOpSepoliaConfig()._appid,
+      helperConfig.getOpSepoliaConfig()._actionId
+    );
+
+    console.log("Identity Manager Address :", address(identitymanager));
+
+    // Deploy the implementation contract
+    GovernautGovernance implementation = new GovernautGovernance();
+
+    // Deploy the ProxyAdmin
+    ProxyAdmin proxyAdmin = new ProxyAdmin(
+      0xfe63Ba8189215E5C975e73643b96066B6aD41A45
+    );
+
+    // Prepare initialization data for GovernautGovernance with the correct IdentityManager address
+    bytes memory initData = abi.encodeWithSelector(
+      GovernautGovernance.initialize.selector,
+      IVotes(governancetoken),
+      address(identitymanager)
+    );
+
+    // Deploy the TransparentUpgradeableProxy with the correct initialization data
+    TransparentUpgradeableProxy proxy = new TransparentUpgradeableProxy(
+      address(implementation),
+      address(proxyAdmin),
+      initData
+    );
+
+    // Treat the proxy as the GovernautGovernance contract
+    governance = GovernautGovernance(payable(address(proxy)));
+
+    console.log("Governaut Governance Address :", address(governance));
+
+    // Deploy the Funding contract
+    funding = new Funding(
+      vm.addr(deployerPrivateKey),
+      address(governance),
+      governancetoken
+    );
+
+    console.log("Funding Address :", address(funding));
+
+    vm.stopBroadcast();
+  }
+}
--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -1,79 +1,98 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-import { Script } from "forge-std/Script.sol";
-import { console } from "forge-std/Test.sol";
-import { WorldIDIdentityManagerRouterMock } from "../test/Anvil/Unit/mocks/WorldIDIdentityManagerRouterMock.sol";
+import {Script} from "forge-std/Script.sol";
+import {console} from "forge-std/Test.sol";
+import {WorldIDIdentityManagerRouterMock} from "../test/Anvil/Unit/mocks/WorldIDIdentityManagerRouterMock.sol";
 
 contract HelperConfig is Script {
-    string appId = "app_staging_6c8d4488699bc14d8d580282ac02b9d5";
-    string actionId = "testing-verfication-action";
+  string appId = "app_staging_6c8d4488699bc14d8d580282ac02b9d5";
+  string actionId = "testing-verfication-action";
+  address GOVERNANCE_TOKEN = address(0);
 
-    struct Config {
-        string _appid;
-        string _actionId;
-        address _WorldcoinRouterAddress;
-    }
+  struct Config {
+    string _appid;
+    string _actionId;
+    address _WorldcoinRouterAddress;
+    address _governanceToken;
+  }
 
-    // function getOpMainnetConfig() public view returns (Config memory) {
-    //     Config memory OpMainnetConfig = Config({
-    //         _appid: appId,
-    //         _actionId: actionId,
-    //         _WorldcoinRouterAddress: vm.envAddress("WORLDCOIN_ROUTERADDRESS_OP_MAINNET")
-    //     });
-    //     return OpMainnetConfig;
-    // }
+  // function getOpMainnetConfig() public view returns (Config memory) {
+  //     Config memory OpMainnetConfig = Config({
+  //         _appid: appId,
+  //         _actionId: actionId,
+  //         _WorldcoinRouterAddress: vm.envAddress("WORLDCOIN_ROUTERADDRESS_OP_MAINNET")
+  //     });
+  //     return OpMainnetConfig;
+  // }
 
-    // function getMetalL2TestnetConfig() public view returns (Config memory) {
-    //     Config memory MetalL2Config = Config({
-    //         _appid: appId,
-    //         _actionId: actionId,
-    //         _WorldcoinRouterAddress: vm.envAddress("WORLDCOIN_ROUTERADDRESS_OP_SEPOLIA")
-    //     });
-    //     return MetalL2Config;
-    // }
+  // function getMetalL2TestnetConfig() public view returns (Config memory) {
+  //     Config memory MetalL2Config = Config({
+  //         _appid: appId,
+  //         _actionId: actionId,
+  //         _WorldcoinRouterAddress: vm.envAddress("WORLDCOIN_ROUTERADDRESS_OP_SEPOLIA")
+  //     });
+  //     return MetalL2Config;
+  // }
+  function getOpMainnetConfig() public view returns (Config memory) {
+    Config memory OpMainnetConfig = Config({
+      _appid: appId,
+      _actionId: actionId,
+      _WorldcoinRouterAddress: vm.envAddress(
+        "WORLDCOIN_ROUTERADDRESS_OP_MAINNET"
+      ),
+      _governanceToken: GOVERNANCE_TOKEN
+    });
+    return OpMainnetConfig;
+  }
 
-    function getOpSepoliaConfig() public view returns (Config memory) {
-        Config memory OpSepoliaConfig = Config({
-            _appid: appId,
-            _actionId: actionId,
-            _WorldcoinRouterAddress: 0x11cA3127182f7583EfC416a8771BD4d11Fae4334
-        });
-        return OpSepoliaConfig;
-    }
+  function getOpSepoliaConfig() public view returns (Config memory) {
+    Config memory OpSepoliaConfig = Config({
+      _appid: appId,
+      _actionId: actionId,
+      _WorldcoinRouterAddress: 0x11cA3127182f7583EfC416a8771BD4d11Fae4334,
+      _governanceToken: GOVERNANCE_TOKEN
+    });
+    return OpSepoliaConfig;
+  }
 
-    // function getEthMainnetConfig() public view returns (Config memory) {
-    //     Config memory EthMainnetConfig = Config({
-    //         _appid: appId,
-    //         _actionId: actionId,
-    //         _WorldcoinRouterAddress: vm.envAddress("WORLDCOIN_ROUTERADDRESS_ETH_MAINNET")
-    //     });
-    //     return EthMainnetConfig;
-    // }
+  // function getEthMainnetConfig() public view returns (Config memory) {
+  //     Config memory EthMainnetConfig = Config({
+  //         _appid: appId,
+  //         _actionId: actionId,
+  //         _WorldcoinRouterAddress: vm.envAddress("WORLDCOIN_ROUTERADDRESS_ETH_MAINNET")
+  //     });
+  //     return EthMainnetConfig;
+  // }
 
-    // function getEthSepoliaConfig() public view returns (Config memory) {
-    //     Config memory EthSepoliaConfig = Config({
-    //         _appid: appId,
-    //         _actionId: actionId,
-    //         _WorldcoinRouterAddress: vm.envAddress("WORLDCOIN_ROUTERADDRESS_ETH_SEPOLIA")
-    //     });
-    //     return EthSepoliaConfig;
-    // }
+  // function getEthSepoliaConfig() public view returns (Config memory) {
+  //     Config memory EthSepoliaConfig = Config({
+  //         _appid: appId,
+  //         _actionId: actionId,
+  //         _WorldcoinRouterAddress: vm.envAddress("WORLDCOIN_ROUTERADDRESS_ETH_SEPOLIA")
+  //     });
+  //     return EthSepoliaConfig;
+  // }
 
-    function getBaseSepoliaConfig() public view returns (Config memory) {
-        Config memory BaseSepoliaConfig = Config({
-            _appid: appId,
-            _actionId: actionId,
-            _WorldcoinRouterAddress: 0x42FF98C4E85212a5D31358ACbFe76a621b50fC02
-        });
-        return BaseSepoliaConfig;
-    }
+  function getBaseSepoliaConfig() public view returns (Config memory) {
+    Config memory BaseSepoliaConfig = Config({
+      _appid: appId,
+      _actionId: actionId,
+      _WorldcoinRouterAddress: 0x42FF98C4E85212a5D31358ACbFe76a621b50fC02,
+      _governanceToken: GOVERNANCE_TOKEN
+    });
+    return BaseSepoliaConfig;
+  }
 
-    function getAnvilConfig() public returns (Config memory) {
-        console.log("testing on anvil");
-        WorldIDIdentityManagerRouterMock routerMock = new WorldIDIdentityManagerRouterMock();
-        Config memory AnvilConfig =
-            Config({ _appid: appId, _actionId: actionId, _WorldcoinRouterAddress: address(routerMock) });
-        return AnvilConfig;
-    }
+  function getAnvilConfig() public returns (Config memory) {
+    console.log("testing on anvil");
+    WorldIDIdentityManagerRouterMock routerMock = new WorldIDIdentityManagerRouterMock();
+    Config memory AnvilConfig = Config({
+      _appid: appId,
+      _actionId: actionId,
+      _WorldcoinRouterAddress: address(routerMock),
+      _governanceToken: GOVERNANCE_TOKEN
+    });
+    return AnvilConfig;
+  }
 }
